68ad5974502cbf01bae22219f7b74214cd23d092
PromentConsulting <webs@promentconsulting.com>
2026-04-23 14:20:38 +0200

    analiticaavanazada

--- analitica_avanzada/lang/en/local_analitica_avanzada.php
+++ analitica_avanzada/lang/en/local_analitica_avanzada.php
@@ -1,6 +1,6 @@
 <?php
 
 $string['pluginname'] = 'Advanced analytics';
-$string['page_title'] = 'Global Analytics';
+$string['page_title'] = '';
 $string['privacy:metadata'] = 'The Advanced analytics plugin does not store personal data.';
 $string['nopermission'] = 'You do not have permissions to access this dashboard.';
